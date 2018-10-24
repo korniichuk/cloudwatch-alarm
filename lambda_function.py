fcedd386b4004c4b80c2db926a1446d481c74407
Ruslan Korniichuk <ruslan.korniichuk@gmail.com>
2018-10-24 17:31:55 +0200

    [cloudwatch-alarm][lambda] Add new 'lambda_function.py' file to the project

---
+++ lambda_function.py
@@ -0,0 +1,45 @@
+import boto3
+import json
+import logging
+import os
+
+from base64 import b64decode
+from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
+
+
+# The base-64 encoded, encrypted key (CiphertextBlob) stored in the kmsEncryptedHookUrl environment variable
+ENCRYPTED_HOOK_URL = os.environ['kmsEncryptedHookUrl']
+# The Slack channel to send a message to stored in the slackChannel environment variable
+SLACK_CHANNEL = os.environ['slackChannel']
+
+HOOK_URL = "https://" + boto3.client('kms').decrypt(CiphertextBlob=b64decode(ENCRYPTED_HOOK_URL))['Plaintext'].decode('utf-8')
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def lambda_handler(event, context):
+    logger.info("Event: " + str(event))
+    message = json.loads(event['Records'][0]['Sns']['Message'])
+    logger.info("Message: " + str(message))
+
+    alarm_name = message['AlarmName']
+    #old_state = message['OldStateValue']
+    new_state = message['NewStateValue']
+    reason = message['NewStateReason']
+
+    slack_message = {
+        'channel': SLACK_CHANNEL,
+        'text': "%s state is now %s: %s" % (alarm_name, new_state, reason)
+    }
+
+    req = Request(HOOK_URL, json.dumps(slack_message).encode('utf-8'))
+    try:
+        response = urlopen(req)
+        response.read()
+        logger.info("Message posted to %s", slack_message['channel'])
+    except HTTPError as e:
+        logger.error("Request failed: %d %s", e.code, e.reason)
+    except URLError as e:
+        logger.error("Server connection failed: %s", e.reason)
