9ce69b06eb289b76c0c4434fb9894ee652c0e212
Ruslan Korniichuk <ruslan.korniichuk@gmail.com>
2018-11-09 20:13:24 +0100

    [cloudwatch-alarm][lambda] Add chart from CloudWatch Metric to Slack Message as png image

--- lambda_function.py
+++ lambda_function.py
@@ -7,17 +7,14 @@ from base64 import b64decode
 from urllib.request import Request, urlopen
 from urllib.error import URLError, HTTPError
 
-
-# The base-64 encoded, encrypted key (CiphertextBlob) stored in the kmsEncryptedHookUrl environment variable
 ENCRYPTED_HOOK_URL = os.environ['kmsEncryptedHookUrl']
-# The Slack channel to send a message to stored in the slackChannel environment variable
 SLACK_CHANNEL = os.environ['slackChannel']
-
-HOOK_URL = 'https://' + boto3.client('kms').decrypt(CiphertextBlob=b64decode(ENCRYPTED_HOOK_URL))['Plaintext'].decode('utf-8')
+HOOK_URL = 'https://' + boto3.client('kms') \
+        .decrypt(CiphertextBlob=b64decode(ENCRYPTED_HOOK_URL))['Plaintext'] \
+        .decode('utf-8')
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-
 
 def lambda_handler(event, context):
 
@@ -26,15 +23,29 @@ def lambda_handler(event, context):
     icon_url = 'https://raw.githubusercontent.com/korniichuk/' \
                'cloudwatch-alarm/master/img/amazon_cloudwatch_icon.png'
     metric ="""{
-        'metrics': [[
-            'LogMetrics',
-            'MetricName',
-            {'period': 3600, 'stat': 'Sum'}]],
-        'title': 'Title',
-        'start': '-P1D',
-        'end': 'P0D',
-        'timezone': '+0100'}"""
+        "metrics": [[
+            "LogMetrics",
+            "MetricName",
+            {"period": 3600, "stat": "Sum"}]],
+        "title": "Title",
+        "start": "-P1D",
+        "end": "P0D",
+        "timezone": "+0100"}"""
+    bucket_name = 'examplebucket' # for temporary public file
+    filename = 'tmp_cloudwatch_metric_chart.png' # of temporary public file
 
+    # Get MetricWidgetImage from CloudWatch Metrics
+    response = boto3.client('cloudwatch') \
+                    .get_metric_widget_image(MetricWidget=metric)
+    data = response['MetricWidgetImage']
+    # Create temporary public file on S3
+    boto3.client('s3').put_object(ACL='public-read', Body=data,
+                                  Bucket=bucket_name, Key=filename)
+    # Get URL of piblic file on S3
+    response = boto3.client('s3').get_bucket_location(Bucket=bucket_name)
+    bucket_location = response['LocationConstraint']
+    image_url = 'https://s3-{0}.amazonaws.com/{1}/{2}'.format(
+            bucket_location, bucket_name, filename)
     logger.info('Event: ' + str(event))
     message = json.loads(event['Records'][0]['Sns']['Message'])
     logger.info('Message: ' + str(message))
@@ -52,7 +63,8 @@ def lambda_handler(event, context):
             {
             'title': 'CloudWatch',
             'value': cloudwatch_url,
-            'short': True}]}]
+            'short': True}],
+        'image_url': image_url}]
     slack_message = {
         'channel': SLACK_CHANNEL,
         'username': 'Amazon CloudWatch',
@@ -69,3 +81,5 @@ def lambda_handler(event, context):
         logger.error('Request failed: %d %s', e.code, e.reason)
     except URLError as e:
         logger.error('Server connection failed: %s', e.reason)
+    # Delete `tmp_cloudwatch_metric_chart.png` public file
+    boto3.client('s3').delete_object(Bucket=bucket_name, Key='chart.png')
